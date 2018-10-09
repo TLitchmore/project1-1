8473e61ac1484e855748929089ddf3cc8d02633a
ashlynfinney <43996272+ashlynfinney@users.noreply.github.com>
2018-10-09 21:00:10 +0200

    Add files via upload

---
+++ lowerBound.java
@@ -0,0 +1,33 @@
+public class LowerBound
+{
+	public static void main(Strings[] args)
+	{
+		//creation of example array to test code
+		int [][] example = {{1,2},{1,3},{1,4},{2,4},{2,5}};
+		
+	}
+	//creation of a method that splits the array into subsets
+	public static int[] subsetMaker(int [][] example)
+	{	
+		int maxConnections = 3;
+		int[][] sub = new int[maxConnections][2];
+		//not necessary, just for the test
+		a = 1;
+		//reading through rows
+		for(i = 0; i<example.length; i++)
+		{
+			//reading through columns
+			for(j = 0; j<example[i].length; j++)
+			{
+				if(example[][0] == a)
+				{
+					example[i][1]
+				}
+				if(example[][1] == a)
+				{
+				
+				}
+			}
+		}
+	}
+}
