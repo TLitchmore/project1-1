51d50380f61cdf9ba2992c342c1b860dd9257729
ashlynfinney <43996272+ashlynfinney@users.noreply.github.com>
2018-10-10 11:25:29 +0200

    Update lowerBound.java

--- lowerBound.java
+++ lowerBound.java
@@ -9,7 +9,9 @@ public class LowerBound
 	//creation of a method that splits the array into subsets
 	public static int[] subsetMaker(int [][] example)
 	{	
+		//comes from the method that finds the max number of connections
 		int maxConnections = 3;
+		//creation of a new MD array to store the subset
 		int[][] sub = new int[maxConnections][2];
 		//not necessary, just for the test
 		a = 1;
@@ -19,14 +21,29 @@ public class LowerBound
 			//reading through columns
 			for(j = 0; j<example[i].length; j++)
 			{
-				if(example[][0] == a)
+				if(example[i][j] == a)
 				{
-					example[i][1]
+					if(j == 0)
+					{
+						//if the a value is in the first column, then we want to know the value in the second column, then j = 1
+						for(int z = 0; z<maxConnections; z++)
+						{
+							sub[z][0] = a;
+							sub[z][1] = example[i][1];
+						}
+
+					}
+					else
+					{
+						// if the a value is in the second column, then we want to know the value in the first column, so j = 0
+						for(int z = 0; z<maxConnections; z++)
+						{
+							sub[z][1] = a;
+							sub[z][0] = example[i][0];
+						}
+					}
 				}
-				if(example[][1] == a)
-				{
 				
-				}
 			}
 		}
 	}
