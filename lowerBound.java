3a7b130cc960a5654c03f99015e78bdaa9a0c726
simooness <43991890+simooness@users.noreply.github.com>
2018-10-10 17:39:09 +0200

    Latest update

--- lowerBound.java
+++ lowerBound.java
@@ -16,9 +16,11 @@ public class LowerBound {
     //creation of a method that splits the array into subsets
     public static int[][] subsetMaker(int[][] example, int a) {
         //comes from the method that finds the max number of connections
-        int maxConnections = 4;
+        int sizeOfSub = example.length;
+        // counter how many direct connections we place in our sub matrix
+        int cntr = 0;
         //creation of a new MD array to store the subset
-        int[][] sub = new int[maxConnections][2];
+        int[][] sub = new int[sizeOfSub][2];
         // z is row in 'sub' matrix (where we place our found values)
         int z = 0;
         //reading through rows
@@ -31,10 +33,12 @@ public class LowerBound {
                         //if the a value is in the first column, then we want to know the value in the second column, so j = 1 and we place these values in the sub matrix
                         sub[z][0] = a;
                         sub[z][1] = example[i][1];
+                        cntr++;
                     } else {
                         // if the a value is in the second column, then we want to know the value in the first column, so j = 0 and we place these values in the sub matrix
                         sub[z][1] = a;
                         sub[z][0] = example[i][0];
+                        cntr++;
                     }
                     // Once we put the first values in the first row, z will refer to the second row after this and so we re-enter the loop
                     z++;
@@ -42,35 +46,43 @@ public class LowerBound {
 
             }
         }
-        // go through rows in sub
-        int p = 0;
-            // go through rows in example
-            for(int q = 0; q<example.length; q++){
-                // go through columns in example
-                for(int r = 0; r<example[0].length; r++){
-                    if(example[q][r] == sub[p][1] ){
-                        // if we find this in the first column AND the value in the second column is the paired value we add this pair to our subset matrix
-                        if(r == 0 && example[q][1] == sub[p+2][1]){
-                            for(int s = 0; s<sub.length; s++){
-                                if(sub[s][0] == 0){
-                                    sub[s][0] = example[q][0];
-                                    sub[s][1] = example[q][1];
-                                }
+        // 
+        for (int firstCompared = 0; firstCompared < cntr - 1; firstCompared++) {
+            for(int secondCompared = 1; secondCompared < cntr; secondCompared++) {
+                compare(firstCompared, secondCompared, example, sub);
+            }
+        }
+
+        return sub;
+    }
+    public static int[][] compare(int rowOfFirstValue, int rowOfSecondValue, int[][] example, int[][] sub) {
+
+        // go through rows in example
+        for (int q = 0; q < example.length; q++) {
+            // go through columns in example
+            for (int r = 0; r < example[0].length; r++) {
+                if (example[q][r] == sub[rowOfFirstValue][1]) {
+                    // if we find this in the first column AND the value in the second column is the paired value we add this pair to our subset matrix
+                    if (r == 0 && example[q][1] == sub[rowOfSecondValue][1]) {
+                        for (int s = 0; s < sub.length; s++) {
+                            if (sub[s][0] == 0) {
+                                sub[s][0] = example[q][0];
+                                sub[s][1] = example[q][1];
                             }
                         }
-                        if(r == 1 && example[q][0] == sub[p+2][1]){
-                            for(int s = 0; s<sub.length; s++){
-                                if(sub[s][0] == 0){
-                                    sub[s][0] = example[q][0];
-                                    sub[s][1] = example[q][1];
-                                }
+                    }
+                    if (r == 1 && example[q][0] == sub[rowOfSecondValue][1]) {
+                        for (int s = 0; s < sub.length; s++) {
+                            if (sub[s][0] == 0) {
+                                sub[s][0] = example[q][0];
+                                sub[s][1] = example[q][1];
                             }
                         }
                     }
                 }
+            }
 
-            } p++;
-
+        }
         return sub;
     }
 
