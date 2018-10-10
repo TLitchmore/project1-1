b2f496e9c63366c83e4b48404764d2a355cd8a6a
simooness <43991890+simooness@users.noreply.github.com>
2018-10-10 16:29:43 +0200

    almost proper subsets working

--- lowerBound.java
+++ lowerBound.java
@@ -1,18 +1,24 @@
+
+
 public class LowerBound {
     public static void main(String[] args) {
         //creation of example array to test code
         int[][] example = {{1, 2}, {1, 3}, {1, 4}, {2, 4}, {2, 5}};
-        printArray(subsetMaker(example));
+        //not necessary, just for the test, a-value is the vertex from which we look at direct connections
+        int a = 1;
+        int[][] sub = subsetMaker(example, a);
+        // int[][] added = addMatrices(example, sub);
+        printArray(sub);
+
+        // System.out.println(findNewA(example, sub, a));
     }
 
     //creation of a method that splits the array into subsets
-    public static int[][] subsetMaker(int[][] example) {
+    public static int[][] subsetMaker(int[][] example, int a) {
         //comes from the method that finds the max number of connections
-        int maxConnections = 3;
+        int maxConnections = 4;
         //creation of a new MD array to store the subset
         int[][] sub = new int[maxConnections][2];
-        //not necessary, just for the test
-        int a = 1;
         // z is row in 'sub' matrix (where we place our found values)
         int z = 0;
         //reading through rows
@@ -36,13 +42,93 @@ public class LowerBound {
 
             }
         }
+        // go through rows in sub
+        int p = 0;
+            // go through rows in example
+            for(int q = 0; q<example.length; q++){
+                // go through columns in example
+                for(int r = 0; r<example[0].length; r++){
+                    if(example[q][r] == sub[p][1] ){
+                        // if we find this in the first column AND the value in the second column is the paired value we add this pair to our subset matrix
+                        if(r == 0 && example[q][1] == sub[p+2][1]){
+                            for(int s = 0; s<sub.length; s++){
+                                if(sub[s][0] == 0){
+                                    sub[s][0] = example[q][0];
+                                    sub[s][1] = example[q][1];
+                                }
+                            }
+                        }
+                        if(r == 1 && example[q][0] == sub[p+2][1]){
+                            for(int s = 0; s<sub.length; s++){
+                                if(sub[s][0] == 0){
+                                    sub[s][0] = example[q][0];
+                                    sub[s][1] = example[q][1];
+                                }
+                            }
+                        }
+                    }
+                }
+
+            } p++;
+
         return sub;
     }
 
-    public static void printArray(int[][] sub) {
-        for (int i = 0; i < sub.length; i++) {
-            for (int j = 0; j < sub[0].length; j++) {
-                System.out.print(sub[i][j] + " ");
+   /* public static int keepPair(ColEdge[] example){
+        ColEdge[]
+    }*/
+
+   /* public static int [][] addMatrices(int[][] example, int[][] sub){
+        int[][] combined = new int[example.length + sub.length][2];
+        // Go through rows in example matrix
+        for(int i = 0; i<example.length; i++) {
+            // Go through columns in example matrix
+            for(int j = 0; j<example[0].length; j++) {
+                combined[i][j] = example[i][j];
+
+                //Go through rows in sub matrix
+                for (int k = 0; k < sub.length; k++) {
+                    // Go through columns in sub matrix
+                    for (int l = 0; l < sub[0].length; l++) {
+                        combined[example.length + k][l] = sub[k][l];
+                    }
+                }
+            }
+        }
+        return combined;
+    }
+    public static int[][] removeDuplicates(int [][] combined) {
+        for(int i = 0; i< combined.length; i++){
+            for(int j = 0; j<combined[0].length; j++){
+
+            }
+    }*/
+
+   /*public static int findNewA(int[][] example, int[][] sub, int a){
+        // Go through rows in example matrix
+        for(int i = 0; i<example.length; i++){
+            // Go through columns in example matrix
+            for(int j = 0; j<example[0].length; j++){
+                int x = example[i][j];
+                //Go through rows in sub matrix
+                for(int k = 0; k<sub.length; k++){
+                    // Go through columns in sub matrix
+                    for(int l = 0; l<sub[0].length; l++){
+                        if(x == sub[k][l]){
+                           // do nothing and go to next value in example matrix
+                        }
+                    }
+                }
+
+            }
+        }
+        return a;
+    }*/
+
+  public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
