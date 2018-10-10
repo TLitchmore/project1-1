770780b10ed7ce3ee4d7c7c66c8d0989ce0b47b4
wafaajbawi <44002795+wafaajbawi@users.noreply.github.com>
2018-10-10 15:41:32 +0200

    Add files via upload

--- ReadGraph.java
+++ ReadGraph.java
@@ -1,18 +1,16 @@
-//# project1-1
-//DKE Project 1-1 group 28 (graph colouring)
 import java.io.*;
 import java.util.*;
 
 		class ColEdge
 			{
-			int u;
+			int u; //e(u,v) adjacent vertices
 			int v;
 			}
 		
 public class ReadGraph
 		{
 		
-		public final static boolean DEBUG = true;
+		public final static boolean DEBUG = false;
 		
 		public final static String COMMENT = "//";
 		
@@ -72,7 +70,7 @@ public class ReadGraph
 
 					e = new ColEdge[m];	
 												
-					for( int d=0; d<m; d++)
+					for( int d=0; d<m; d++) //0 is less than the number of edges
 						{
 						if(DEBUG) System.out.println(COMMENT + " Reading edge "+(d+1));
 						record = br.readLine();
@@ -123,75 +121,48 @@ public class ReadGraph
 			//! 
 			//! there will be n vertices in the graph, numbered 1 to n
 
-			int[] c= new int[n]; //array to count the number of connections a vertice has
-			boolean[][] color=new boolean[n][n]; //array to arrange the vertices into colors (each color is a number, according to placements in the array)
-			count(c, e, 0);
-			System.out.println("the number of the upperbound connection: " + (assupper(c, 0) +1) );
+			//! INSERT YOUR CODE HERE!
+			//assign color to all vertices from 1 to n vertices 
 				
-			System.out.println("the number of the Chromatic connection: " + (asschro(e, color) +1));
+			int[] result = new int[n]; //create new array to store the result ,, its length the same as the number of vertices 
 			
-		}
-
-		public static int[] count(int[] c, ColEdge[] e, int i) //i is for placement in array c
-		{
-			if (i == e.length) 
-				return c; //returns the full array
-			for (int j=0; j<e.length; j++) //j for placement in array e
-				/* every time a vertice appears in the list it means it has a connection,
-					so every time we see the vertice we add +1 (counting another connection)
-					to the right placement in the counting array, so, if vertice 3 has another
-					connection we add 1 to place 2 (because the array is 0 to n-1 while the 
-					vertices are 1 to n) to mark another connection */
-				if (e[j].u == i+1 || e[j].v == i+1)
-					c[i]++;
-			return count(c, e, i+1); 
-		}
-		
-		public static int assupper(int[] c, int biggest)
-		{
-			for (int i=0; i<c.length; i++) //finds the vertice with most connections 
-				if (c[i]> biggest)
-					biggest= c[i];
-			return biggest;
-		}
-
-		
-		public static int asschro(ColEdge[] e, boolean[][] color)
-		{
-			color[e[0].u][0]=true;
-			for (int i=0;i<e.length;i++)
-			{
-				for (int j=0; j<color.length; j++)
-				{
-					if (color[e[i].u-1][j]==true)
-						color[e[i].v-1][j+1]=true;
-					else
-						color[e[i].v-1][j]=true;
-					if (color[e[i].v-1][j]==true)
-						color[e[i].u-1][j+1]=true;
-					else
-						color[e[i].u][j]=true;
-				}
+			 result[0] = 1; //assign the first color to the first vertix 
+			 
+			boolean[] availableColor = new boolean[n];
+			//create a temporary array for the available colors. 
+			
+		int	chromaticNumber = assignColor(e , result, availableColor);
+		  System.out.println( chromaticNumber);
 			}
-			int n=-1;
-			for (int j=color.length-1; j>-1; j--)
-				if (color[0][j]!=true)
-				{
-					n=j;
-					return j;
-				}
-			return n;
-		}		
+			public static int assignColor(ColEdge[] pairs ,int[] result , boolean[] availableColor ){ 
+			
+			
+		//	for ( int i = 0; i < result.length-1; i++){ 
+				//go over the remaining vertices 
+				
+				if(result[0] == result[1] && pairs[0].u == pairs[1].v){
+					//if the first color is  the same as the the next color of the second vertix and they are adjacent
+				result[1] = result[0]++;
+				// new color depend on the previous condiditions 
+			//	}
+			
+			}
+						return result[0];
 
-}
-
-
-
-/*
-notes !!
-
-				if (e[i].u == e[j].u || e[i].u == e[j].v])
-					c[e[i].u]++;
-				if  (e[i].v == e[j].u || e[i].v == e[j].v)
-					c[e[i].v]++;
-*/
+			}
+			
+	/*	}
+			if (result[0] ==1) //recursively go through every vertex in the graph
+				return 1
+			else {if (result[i+1] != result[0])
+				{assignColor( pairs , result , availableColor)}
+		}
+			
+			}
+		*/
+		
+		
+	
+		}
+			
+			
